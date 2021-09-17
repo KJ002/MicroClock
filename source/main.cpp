343d8a20285b905811bb38585c8b850f32b55a79
James Butcher <jamesbutcher167@gmail.com>
2021-09-17 20:33:47 +0100

    Write a cute heart to the screen

--- source/main.cpp
+++ source/main.cpp
@@ -6,7 +6,14 @@ int main()
 {
 	uBit.init();
 
-	uBit.display.scroll("HELLO WORLD!");
+	 const uint8_t heart[] = { 0, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0}; // a cute heart
+	 MicroBitImage i(5,5,heart);
+
+	while (true){
+		uBit.display.print(i);
+		uBit.sleep(500);
+	}
 
 	release_fiber();
+	return 0;
 }
