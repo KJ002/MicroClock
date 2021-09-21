e6fcd5a6fb57393d58f72daeb2ddb9fbc69adaaf
James Butcher <jamesbutcher167@gmail.com>
2021-09-21 21:43:53 +0100

    Rotating line around origin

--- source/main.cpp
+++ source/main.cpp
@@ -130,16 +130,25 @@ uint8_t * Vec2uBitImage(Vec2 start, Vec2 end, float th = 0){
 	return matrix;
 }
 
+
+
 int main()
 {
 	uBit.init();
 
-	uint8_t * data = Vec2uBitImage((Vec2){2, 2}, (Vec2){2, 4}, 90);
+	float rotation = 0;
 
-	MicroBitImage k(5, 5, data);
+	uint8_t * data;
+
+	MicroBitImage k;
+
 	while (true){
+		data = Vec2uBitImage((Vec2){2, 2}, (Vec2){2, 4}, rotation);
+		k = MicroBitImage(5, 5, data);
 		uBit.display.print(k);
 		uBit.sleep(250);
+		rotation++;
+		rotation = (int)rotation%360;
 	}
 
 	release_fiber();
