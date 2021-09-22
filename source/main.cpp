8f89d5501696d1a1c8eac3e505b586fe5ca84235
James Butcher <jamesbutcher167@gmail.com>
2021-09-22 20:13:46 +0100

    Add vec2 header file to main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -1,81 +1,14 @@
 #include "MicroBit.h"
+#include "vec2.hpp"
 #include <cmath>
+
+
 
 #define MAP_X 5
 #define MAP_Y 5
 #define MAP0_X 4
 #define MAP0_Y 4
 
-
-struct Vec2{
-	float x;
-	float y;
-
-	Vec2 operator+(const Vec2& other) const{
-		return (Vec2){x+other.x, y+other.y};
-	}
-
-	Vec2 operator-(const Vec2& other) const{
-		return (Vec2){x-other.x, y-other.y};
-	}
-
-	Vec2 operator*(const Vec2& other) const{
-		return (Vec2){x*other.x, y*other.y};
-	}
-
-	Vec2 operator/(const Vec2& other) const{
-		return (Vec2){x/other.x, y/other.y};
-	}
-
-	bool operator>(const Vec2& other) const{
-		if ((x*y) > (other.x*other.y))
-			return true;
-		return false;
-	}
-
-	bool operator<(const Vec2& other) const{
-		return !operator>(other);
-	}
-
-	double dotProduct(const Vec2& other) const{
-		return x*other.x+y*other.y;
-	}
-
-	double crossProduct(const Vec2& other) const{
-		return x+other.x*y+other.y;
-	}
-
-	Vec2 rotate(const double th) const{
-
-		if (!th%360)
-			return *this;
-
-		const double rad = degToRad(360-th);
-
-		Vec2 result;
-
-		result.x = std::cos(rad) * this->x - std::sin(rad) * this->y;
-		result.y = std::sin(rad) * this->x + std::cos(rad) * this->y;
-
-		return result;
-	}
-
-	Vec2 rotate(const double th, const Vec2& origin) const{
-		Vec2 newPoint = *this - origin;
-
-		return newPoint.rotate(th)+origin;
-	}
-
-	float length() const{
-		return std::sqrt(power2(x) + power2(y));
-	}
-
-	Vec2 round() const{
-		Vec2 result = {_round(x), _round(y)};
-
-		return result;
-	}
-};
 
 MicroBit uBit;
 
