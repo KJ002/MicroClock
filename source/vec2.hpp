c5b70b7972725f7066c607bdfdf1fb76584b05c0
James Butcher <jamesbutcher167@gmail.com>
2021-09-22 19:58:44 +0100

    Add Vec2 header file

---
+++ source/vec2.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+struct Vec2{
+    float x;
+    float y;
+
+    Vec2 operator+(const Vec2& other) const;
+    Vec2 operator-(const Vec2& other) const;
+    Vec2 operator*(const Vec2& other) const;
+    bool operator>(const Vec2& other) const;
+    bool operator<(const Vec2& other) const;
+    double dotProduct(const Vec2& other) const;
+    double crossProduct(const Vec2& other) const;
+    Vec2 rotate(const double th) const;
+    Vec2 rotate(const double th, const Vec2& origin) const;
+    float length() const;
+    Vec2 round() const;
+};
